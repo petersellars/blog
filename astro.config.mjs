36f7ab86774865d9ec7a3f4c756acf81b3a7e9a7
Peter Sellars <psellars@gmail.com>
2024-11-27 18:13:11 +1300

    feat(island): Initial Preact Greeting
    * Add Preact Integration
    * Fix Dependencies
    * Add Island Greeting
    * Use Island Greeting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://ubiquitous-syrniki-3c3727.netlify.app/"
+  site: "https://ubiquitous-syrniki-3c3727.netlify.app/",
+  integrations: [preact()]
 });
