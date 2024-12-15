31c8e4768cb3fe34a064d0124fd1bd330520b0f2
Peter Sellars <psellars@gmail.com>
2024-12-15 18:45:42 +1300

    feat(contentful): attempt to add contentful to blog - UNSUCCESSFUL

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+interface ImportMetaEnv {
+    readonly CONTENTFUL_SPACE_ID: string;
+    readonly CONTENTFUL_DELIVERY_TOKEN: string;
+    readonly CONTENTFUL_PREVIEW_TOKEN: string;
+    readonly DEV: boolean;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
